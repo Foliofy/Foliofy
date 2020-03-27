3a66de4d601d2c106d104b8df6e17d0bdc478158
Anmol Gupta <anmolify@gmail.com>
2020-03-27 14:22:45 +0530

    Updating src

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Form.css";
 import Personal from "../Form/FormComponents/PersonalCompoenent/PersonalComponent";
+import Education from "../Form/FormComponents/EducationComponent/EducationComponent";
 const form = () => {
   return (
     <React.Fragment>
-      <Personal />
+      <Education />
     </React.Fragment>
   );
 };
