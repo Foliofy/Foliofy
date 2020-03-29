896bf91f78c46a61bc8dadf6ad54e77fb8ff3a89
Anmol Gupta <anmolify@gmail.com>
2020-03-29 15:41:52 +0530

    Footer Screen Design

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,6 +3,7 @@ import Cover from "./PortfolioComponents/CoverScreen/CoverScreen"
 import AboutMe from "./PortfolioComponents/AboutMeScreen/AboutMeScreen"
 import Education from "./PortfolioComponents/EducationScreen/EducationScreen"
 import Experience from "./PortfolioComponents/ExperienceScreen/ExperienceScreen"
+import Footer from "./PortfolioComponents/FooterScreen/Footer"
 const portfolio = () => (
     <React.Fragment>
         <Cover />
@@ -11,6 +12,7 @@ const portfolio = () => (
         <Education />
         <hr/>
         <Experience />
+        <Footer />
     </React.Fragment>
 )
 
---
+++ src/components/Portfolio/PortfolioComponents/FooterScreen/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+import {Navbar} from "react-bootstrap";
+import linkedIn from "/Users/apple/Desktop/FoliofyOrg/src/assets/images/linked.png"
+const footerPort = (props) => {
+    return (
+        <Navbar bg="dark" variant="dark" fixed="bottom">
+        <Navbar.Brand href={props.prsnl.linkedinUrl}>
+          <img
+            src={linkedIn}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          LinkedIn 
+        </Navbar.Brand>
+      </Navbar>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        prsnl: state.personal
+    }
+}
+
+export default connect(mapStateToProps)(footerPort);
